b42f2344f3f86d9915f56230ac23ffaa4f03bf01
Haippp <haiperdesain@gmail.com>
2026-04-27 11:34:27 +0800

    fix: perubahan nama controller

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AntrianController;
 use App\Http\Controllers\Api\AsesmenController;
-use App\Http\Controllers\Api\DiagnosaController;
+use App\Http\Controllers\Api\RekamMedikController;
 use App\Http\Controllers\Api\ResepController;
 
 Route::get('/antrian/{id_poli}', [AntrianController::class, 'index']);
 Route::patch('/antrian/{id}/panggil',[AntrianController::class, 'panggil']);
 
 Route::post('/asesmen', [AsesmenController::class, 'store']);
-Route::post('/Diagnosa', [DiagnosaController::class, 'store']);
+Route::post('/Diagnosa', [RekamMedikController::class, 'store']);
 
 Route::post('/resep', [ResepController::class, 'store']);
 Route::delete('/resep/id', [ResepController::class, 'destroy']);
 
-Route::get('/tagihan', [DiagnosaController::class, 'lihatTagihan']);
+Route::get('/tagihan', [RekamMedikController::class, 'lihatTagihan']);
