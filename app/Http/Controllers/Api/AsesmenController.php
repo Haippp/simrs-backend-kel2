cd94b5fcaeb787e165a69b9113d167398d4b04d4
Haippp <haiperdesain@gmail.com>
2026-04-27 16:20:02 +0800

    feat: penambahan swagger untuk dokumentasi API

--- app/Http/Controllers/Api/AsesmenController.php
+++ app/Http/Controllers/Api/AsesmenController.php
@@ -8,13 +8,41 @@ use Illuminate\Http\Request;
 
 class AsesmenController extends Controller
 {
+    /**
+     * @OA\Post(
+     * path="/asesmen",
+     * operationId="storeAsesmen",
+     * tags={"Asesmen Perawat"},
+     * summary="Sipman data asesmen awal (TTV)",
+     * description="Menyimpan data keluhan, tensi, suhu yang diinput oleh perawat.",
+     * @OA\RequestBody(
+     * required=true,
+     * @OA\JsonContent(
+     * required={"id_antrian", "id_pasien", "keluhan_utama", "tensi", "suhu", "nadi", "respirasi", "tinggi_badan", "berat_badan"},
+     * @OA\Property(property="id_antrian", type="integer", example=1),
+     * @OA\Property(property="id_pasien", type="integer", example=99),
+     * @OA\Property(property="keluhan_utama", type="string", example="Nyeri dada"),
+     * @OA\Property(property="tensi", type="string", example="120/80"),
+     * @OA\Property(property="suhu", type="number", format="float", example=36.4),
+     * @OA\Property(property="nadi", type="integer", example=80),
+     * @OA\Property(property="respirasi", type="integer", example=20),
+     * @OA\Property(property="tinggi_badan", type="number", format="float", example=165.6),
+     * @OA\Property(property="berat_badan", type="number", format="float", example=55.5),
+     * )
+     * ),
+     * @OA\Response(
+     * response=201,
+     * description="Berhasil menyimpan data"),
+     * @OA\Response(response=422, description="Validasi Error")
+     * )
+     */
     public function store(Request $request){
         $validate = $request->validate([
             'id_antrian' => 'required|integer',
             'id_pasien' => 'required|integer',
             'keluhan_utama' => 'required|string',
             'tensi' => 'required|string',
-            'suhu' => 'required|numeric|betweeen:0,45',
+            'suhu' => 'required|numeric|between:0,45',
             'nadi' => 'required|integer|min:0',
             'tinggi_badan' => 'required|numeric|min:0',
             'berat_badan' => 'required|numeric|min:0'
--- app/Http/Controllers/Api/RekamMedikController.php
+++ app/Http/Controllers/Api/RekamMedikController.php
@@ -7,6 +7,35 @@ use Illuminate\Http\Request;
 
 class RekamMedikController extends Controller
 {
+    /**
+     * @OA\Post(
+     * path="/rekam-medik",
+     * operationId="storeRekamMedik",
+     * tags={"Rekam Medik Dokter"},
+     * summary="Simpan data rekam medik (Diagnosa & Tindakan)",
+     * description="Menyimpan hasil pemeriksaan, diagnosa, dan tindakan medis yang diinput oleh dokter.",
+     * @OA\RequestBody(
+     * required=true,
+     * @OA\JsonContent(
+     * required={"id_antrian", "id_pasien", "id_dokter", "diagnosa", "tindakan_medis"},
+     * @OA\Property(property="id_antrian", type="integer", example=1),
+     * @OA\Property(property="id_pasien", type="integer", example=99),
+     * @OA\Property(property="id_dokter", type="integer", example=5),
+     * @OA\Property(property="diagnosa", type="string", example="Hipertensi Grade II"),
+     * @OA\Property(property="tindakan_medis", type="string", example="Pemeriksaan EKG + Konsultasi Gizi")
+     * )
+     * ),
+     * @OA\Response(
+     * response=201,
+     * description="Berhasil menyimpan data rekam medik",
+     * @OA\JsonContent(
+     * @OA\Property(property="status", type="string", example="success"),
+     * @OA\Property(property="message", type="string", example="Rekam medik pasien berhasil disimpan")
+     * )
+     * ),
+     * @OA\Response(response=422, description="Validasi Error")
+     * )
+     */
     public function store(Request $request){
         $validate = $request->validate([
             'id_antrian' => 'required|integer',
