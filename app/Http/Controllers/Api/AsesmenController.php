f47f3ce25e74e698ba62d1e0d7494c7c06e64aa7
Haippp <haiperdesain@gmail.com>
2026-04-27 12:45:37 +0800

    feat: penambahan controller untuk api asesmen

---
+++ app/Http/Controllers/Api/AsesmenController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use GrahamCampbell\ResultType\Success;
+use Illuminate\Http\Request;
+
+class AsesmenController extends Controller
+{
+    public function store(Request $request){
+        $validate = $request->validate([
+            'id_antrian' => 'required|integer',
+            'id_pasien' => 'required|integer',
+            'keluhan_utama' => 'required|string',
+            'tensi' => 'required|string',
+            'suhu' => 'required|numeric|betweeen:0,45',
+            'nadi' => 'required|integer|min:0',
+            'tinggi_badan' => 'required|numeric|min:0',
+            'berat_badan' => 'required|numeric|min:0'
+        ]);
+
+        // $asesmen = Asesmen::create($validate)
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Data asesmen awal berhasil disimpan'
+        ], 201);
+    }
+}
